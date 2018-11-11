4f4d17d8ee59df6aa1c4237d902d2e30da2b2337
Kingluddite <howley.phil@gmail.com>
2018-11-10 17:19:59 -0800

    Hash password and create user

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -30,6 +33,34 @@ const Mutations = {
       },
       info
     );
+  },
+
+  async signup(parent, args, ctx, info) {
+    // make sure all emails are lowercase
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the db
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
+    // create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 24 * 365,
+    });
+    // User created and we have a token
+    // now return the user to the browser
+    return user;
   },
 };
 
